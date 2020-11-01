c4b8c756781f7bd876962c07f0503b86ad1b66da
rnfor-pro <nfor.rudolph1@gmail.com>
2020-11-01 15:44:26 -0500

    .

--- deployed.sh
+++ deployed.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo List of 2021 DevOps engineer
 echo Simon Elvis
-echo Stanley Kenedy
+echo Stanley Kenedy Kendy
+
+
--- users.java
+++ users.java
@@ -1,4 +1,3 @@
-mofor
 simon
 sylvie
 eric
@@ -6,3 +5,4 @@ harmless
 Line
 Light
 forgiveness
+Kenedy
