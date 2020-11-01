e2035c8ff0a869ac5d2a4f4e0d21f2778f1cf3ed
rnfor-pro <nfor.rudolph1@gmail.com>
2020-11-01 15:49:55 -0500

    Database updated

--- users.java
+++ users.java
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-=======
 #List of DevOps Engineers
 mofor
->>>>>>> dfcd679d773c7a5e2e482fdd922fd9d9fff1a83c
 simon
-sylvie
 eric
 harmless
 Line
 Light
 forgiveness
-<<<<<<< HEAD
 Kenedy
-=======
 Bintu
 Marielle
->>>>>>> dfcd679d773c7a5e2e482fdd922fd9d9fff1a83c
